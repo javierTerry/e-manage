bc144615dc53a16ba47faac68f5ee9c62ec478e2
Javier Hernandez <javierv31@gmail.com>
2023-11-02 21:51:59 -0600

    Menu:cliente - submenu:oferta - descripcion: se agregan tablas de resumen

---
+++ application/views/deal/ofertas/paso2/tablaCCPaso2RoiResumen.php
@@ -0,0 +1,13 @@
+<table id="tablaCCPaso2RoiResumen" class="table table-striped table-bordered dt-responsive nowrap hover cursor-picker" cellspacing="0" width="100%" style="display: none;">
+    <thead> 
+        <tr BGCOLOR ="#86a899"> <th  colspan="4" style="text-align:center">ROI</th></tr>
+        <tr>    
+            <th style="text-align:center">Ahorro anual (MXN)</th>  
+            <th style="text-align:center">Ahorro mensual (MXN)    </th>  
+            <th style="text-align:center">Inversión (MXN)</th>  
+            <th style="text-align:center">Periodo de retorno de la inversión (meses)</th>  
+        </tr>   
+    </thead>    
+    <tbody>
+    </tbody>    
+</table>
---
+++ application/views/deal/ofertas/paso2/tablaCCPaso2UtilidadesResumen.php
@@ -0,0 +1,33 @@
+<table id="tablaCCPaso2UtilidadesResumen" class="table table-striped table-bordered dt-responsive nowrap hover cursor-picker" cellspacing="0" width="100%" style="display: none;">
+        <thead> 
+            <tr BGCOLOR ="#86a899"> <th  colspan="13" style="text-align:center">UTILIDADES</th></tr>
+            <tr BGCOLOR ="#86a899"> 
+                <th  colspan="7" style="text-align:center">Utilidad Fénix</th> 
+                <th  colspan="6" style="text-align:center">Intermediario</th> 
+            </tr>
+            <tr BGCOLOR ="#86a899"> 
+                <th  colspan="4" style="text-align:center">Cobertura 100% NEC</th> 
+                <th  colspan="3" style="text-align:center">Cobertura 60% NEC 40% Spot</th> 
+                <th  colspan="3" style="text-align:center">Cobertura 100% NEC </th> 
+                <th  colspan="3" style="text-align:center">Cobertura 60% NEC 40% Spot</th> 
+            </tr>
+            <tr BGCOLOR ="#86a899"> 
+                <th style="text-align:center">Consumo <p>+ PPT<p>+ PPNT (kWh)</th> 
+                <th style="text-align:center">Total Utilidad<p> (MXN/MWh)</th> 
+                <th style="text-align:center">% Utilidad </th> 
+                <th style="text-align:center">Utilidad <p>(MXN)</th> 
+                <th style="text-align:center">Total Utilidad<p> (MXN/MWh)</th> 
+                <th style="text-align:center">% Utilidad <p>Fenix </th> 
+                <th style="text-align:center">Utilidad <p>(MXN)</th> 
+                <th style="text-align:center">Fee <p>(MXN/MWh)</th> 
+                <th style="text-align:center">% Fee sobre<p> utilidad</th> 
+                <th style="text-align:center">Total fee <p>(MXN)</th> 
+                <th style="text-align:center">Fee<p> (MXN/MWh)</th> 
+                <th style="text-align:center">% Fee <p>sobre utilidad</th> 
+                <th style="text-align:center">Total fee<p> (MXN)</th> 
+            </tr>
+        </thead>    
+        <tbody>
+         
+        </tbody>    
+    </table>
